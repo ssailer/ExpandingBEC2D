f55191ba77cdba06cd93c91f4b2ecc23e4c6f74f
nhrjr <jester.raced@gmail.com>
2014-09-29 18:16:49 +0200

    itp.cpp settings'

--- itp.cpp
+++ itp.cpp
@@ -59,12 +59,12 @@ try{
 	Options tmpOpt = startUp.getOptions();
 	MatrixData* startGrid = new MatrixData(1,tmpOpt.grid[1],tmpOpt.grid[2],0,0,tmpOpt.min_x,tmpOpt.min_y);
 
-	setGridToGaussian(startGrid,startUp.getOptions());
+	// setGridToGaussian(startGrid,startUp.getOptions());
 
-	// string groundStateName = "StartGrid_2048x2048_N1000_groundState.h5";
-	// binaryFile* groundStateFile = new binaryFile(groundStateName,binaryFile::in);
-	// groundStateFile->getSnapshot("StartGrid",0,startGrid,tmpOpt);
-	// delete groundStateFile;
+	string groundStateName = "StartGrid_2048x2048_N1000_groundState.h5";
+	binaryFile* groundStateFile = new binaryFile(groundStateName,binaryFile::in);
+	groundStateFile->getSnapshot("StartGrid",0,startGrid,tmpOpt);
+	delete groundStateFile;
 		
 
 
@@ -74,22 +74,22 @@ try{
 	// startGrid->wavefunction[0] = groundStateITP->result();
 	// delete groundStateITP;
 
-	// 	int vnumber = 0;
-	// 	addVorticesAlternating(startGrid,startUp.getOptions(),vnumber);
+		int vnumber = 0;
+		addVorticesRegular(startGrid,startUp.getOptions(),vnumber);
 		
-	// 	startUp.setVortexnumber(vnumber);
-	// 	cout << endl << "Set Vortices #: " << vnumber << endl;
+		startUp.setVortexnumber(vnumber);
+		cout << endl << "Set Vortices #: " << vnumber << endl;
 	
-	// 	string itpname = "ITP-Vortices";
-	// 	ITP* vorticesITP = new ITP(startGrid->wavefunction[0],startUp.getOptions());
-	// 	vorticesITP->formVortices(itpname);
+		string itpname = "ITP-Vortices";
+		ITP* vorticesITP = new ITP(startGrid->wavefunction[0],startUp.getOptions());
+		vorticesITP->formVortices(itpname);
 		
-	// 	startGrid->wavefunction[0] = vorticesITP->result();
+		startGrid->wavefunction[0] = vorticesITP->result();
 	
-	// 	delete vorticesITP;
+		delete vorticesITP;
 
 
-	string startGridName = "StartGrid_2048x2048_N1000_GaussDistribution.h5";
+	string startGridName = "StartGrid_2048x2048_N1000_WN2_sV_regular.h5";
 	binaryFile* dataFile = new binaryFile(startGridName,binaryFile::out);
 	dataFile->appendSnapshot("StartGrid",0,startGrid,tmpOpt);
 	delete dataFile;
