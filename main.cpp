9d099c035030a85be1906b14cb4d6e6ba1e57d69
nhrjr <jester.raced@gmail.com>
2014-11-21 01:35:47 +0100

    stuff

--- main.cpp
+++ main.cpp
@@ -74,7 +74,6 @@ try{
 		delete dataFile;
 
 
-
 			// Options tmpOpt = startUp.getOptions();
 			// MatrixData* startGrid = new MatrixData(1,tmpOpt.grid[1],tmpOpt.grid[2],0,0,tmpOpt.min_x,tmpOpt.min_y);
 	
