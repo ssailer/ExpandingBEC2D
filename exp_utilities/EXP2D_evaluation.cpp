23e3fb559d12f19121be57ffd06705716c9514c6
nhrjr <jester.raced@gmail.com>
2014-12-03 20:34:10 +0100

    itp

--- exp_utilities/EXP2D_evaluation.cpp
+++ exp_utilities/EXP2D_evaluation.cpp
@@ -718,7 +718,7 @@ void Eval::getDensity(ComplexGrid &data, RealGrid &densityLocationMap_local, vec
 
 	double h_x = 2. * opt.stateInformation[0] * opt.min_x / opt.grid[1];
 	double h_y = 2. * opt.stateInformation[1] * opt.min_y / opt.grid[2];
-	double threshold = opt.N * 0.05 / (4. * opt.min_x * opt.stateInformation[0] * opt.min_y * opt.stateInformation[1]);  //abs2(data(0,opt.grid[1]/2,opt.grid[2]/2,0))*0.9; 
+	double threshold = opt.N * 0.01 / (4. * opt.min_x * opt.stateInformation[0] * opt.min_y * opt.stateInformation[1]);  //abs2(data(0,opt.grid[1]/2,opt.grid[2]/2,0))*0.9; 
 
 	// RealGrid densityLocationMap = RealGrid(opt.grid[0],opt.grid[1],opt.grid[2],opt.grid[3]);
 	densityLocationMap_local = RealGrid(opt.grid[0],opt.grid[1],opt.grid[2],opt.grid[3]);
